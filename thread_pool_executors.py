250a6197efdcec8460a37741ba8c8a6d6e2b3e63
Dane <danesherbs@gmail.com>
2022-09-17 21:36:47 -0700

    Create thread_pool_executors.py

---
+++ thread_pool_executors.py
@@ -0,0 +1,40 @@
+# %%
+
+from time import time, sleep
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+# %%
+
+with ThreadPoolExecutor() as executor:
+    future = executor.submit(pow, 2, 3)
+    future.add_done_callback(lambda f: print(f.result()))
+
+# %%
+
+with ThreadPoolExecutor() as executor:
+    futures = [executor.submit(pow, 2, n) for n in [1, 2, "invalid input"]]
+
+for future in as_completed(futures):
+    try:
+        print(future.result())
+    except Exception as exc:
+        print("Generated exception:", exc)
+
+# %%
+
+with ThreadPoolExecutor() as executor:
+    for result in executor.map(lambda x: x ** 2, [1, 2, 3]):
+        print(result)
+
+# %%
+
+start = time()
+[sleep(1) for _ in range(3)]
+print(f"Single threaded: completed in {time() - start:.2f} seconds")
+
+start = time()
+with ThreadPoolExecutor() as executor:
+    executor.map(lambda _: sleep(1), range(3))
+print(f"Multi-threaded: completed in {time() - start:.2f} seconds")
+
+# %%
