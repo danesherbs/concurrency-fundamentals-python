ee26eb12d0ae45f35fa15284942ac30da0c23031
Dane <danesherbs@gmail.com>
2022-09-18 17:34:08 -0700

    Update threads.py

--- threads.py
+++ threads.py
@@ -33,7 +33,14 @@ lock.release()
 
 # %%
 
-rlock = threading.RLock()
+with lock:
+    print(lock.locked())  # True
+
+lock.locked()  # False
+
+# %%
+
+rlock = threading.RLock()  # allows a thread to acquire the lock multiple times
 
 rlock.acquire()  # True
 rlock.acquire(timeout=1.0)  # True (since same thread (main) can aquire it multiple times; also increments recursion level)
@@ -42,7 +49,7 @@ rlock.release()  # recursion level zero; can now be aquired by another thread
 
 # %%
 
-sem = threading.Semaphore(3)  # useful when size of resource is fixed e.g. num connections to database
+sem = threading.Semaphore(3)
 
 sem.release()
 sem._value  # 4
@@ -55,13 +62,13 @@ sem.acquire(timeout=1.0)  # False (due to timeout)
 
 # %%
 
-sem = threading.BoundedSemaphore(3)
+sem = threading.BoundedSemaphore(3)  # useful when size of resource is fixed e.g. num connections to database
 
 sem.release()  # error (released too many times)
 
 # %%
 
-event = threading.Event()
+event = threading.Event()  # allows communicatation between two threads
 
 def countdown():
     for i in [3, 2, 1]:
@@ -76,5 +83,29 @@ def race():
 
 threading.Thread(target=countdown).start()
 threading.Thread(target=race).start()
+
+# %%
+
+timer = threading.Timer(5.0, lambda: print("5 second timer up!"))
+timer.start()  # Timer is a subclass of Thread; it's run method is invoked by calling start()
+
+# %%
+
+barrier = threading.Barrier(parties=2, timeout=5.0)  # a way to synchronise threads
+
+def server():
+    sleep(1.0)
+    barrier.wait()
+    # connection with client gauranteed here
+    print("server: passed the barrier")
+
+def client():
+    sleep(2.0)
+    barrier.wait()
+    # connection with server gauranteed here
+    print("client: passed the barrier")
+
+threading.Thread(target=server).start()
+threading.Thread(target=client).start()
 
 # %%
